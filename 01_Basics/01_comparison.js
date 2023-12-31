9992866af56703ba34768ceaee5d9cee19690c9b
avbhutani <avbhutani3@gmail.com>
2024-01-01 01:23:56 +0530

    Made a new file 01_Basics/01_comparison.js

---
+++ 01_Basics/01_comparison.js
@@ -0,0 +1,10 @@
+console.log('A' < 'a')
+
+// Now we are using the ASCII values to compare all the types of data.
+// Js allows us to compare the strings using the ASCII values.
+// Whereas in typescript we cannot compare the strings using the ASCII values.
+// In typescript we can only compare the strings using the alphabetical order.
+
+console.log(null === 0)
+console.log(null <= 0)
+console.log(null == 0)
