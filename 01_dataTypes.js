d1fce481afd8cfc8325e4df0703c986f046e7cf0
avbhutani <avbhutani3@gmail.com>
2024-01-01 00:51:08 +0530

    Made changes to the 01_dataTypes.js file

--- 01_dataTypes.js
+++ 01_dataTypes.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+let score = null
+
+
+score = Number(score)
+console.log(score)
+
+
+let isLoggedIn = 0
+console.log(isLoggedIn = Boolean(isLoggedIn))
+// So assignment bhi hori hai aur print bhi hora hai result
+console.log(isLoggedIn)
+
+// Data types me conversion ho skta hai agar kuch bhi ho variable ke andar to true ho jayega. 
